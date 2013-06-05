e1a97f81c7b567d4422ad45c713445d906e6e020
Vitaly Kushner <vitaly@astrails.com>
2013-06-05 15:55:39 +0300

    DRY

--- lib/lazyeval.rb
+++ lib/lazyeval.rb
@@ -4,53 +4,25 @@ class Object
   end
 end
 
-if RUBY_VERSION < '1.9'
+class Lazy < (::RUBY_VERSION < '1.9') ? Object : BasicObject
+  undef_method(*(instance_methods - %w/__id__ __send__ respond_to? debugger/)) if ::RUBY_VERSION < '1.9'
 
-  class Lazy
-    undef_method(*(instance_methods - %w/__id__ __send__ respond_to? debugger/))
-
-    def initialize(obj, &block)
-      @obj, @block = obj, block
-      @args = @done = nil
-    end
-
-    def method_missing(*args, &block)
-      unless @done
-        if !@args && !@block
-          @args, @block = args, @block
-          return self
-        end
-
-        @res = @args ?  @obj.send(*@args, &@block) : @block.call(@obj)
-        @done = true
-      end
-
-      @res.send(*args, &block)
-    end
+  def initialize(obj, &block)
+    @obj, @block = obj, block
+    @args = @done = nil
   end
 
-else
-
-  class Lazy < BasicObject
-
-    def initialize(obj, &block)
-      @obj, @block = obj, block
-      @args = @done = nil
-    end
-
-    def method_missing(*args, &block)
-      unless @done
-        if !@args && !@block
-          @args, @block = args, @block
-          return self
-        end
-
-        @res = @args ?  @obj.send(*@args, &@block) : @block.call(@obj)
-        @done = true
+  def method_missing(*args, &block)
+    unless @done
+      if !@args && !@block
+        @args, @block = args, @block
+        return self
       end
 
-      @res.send(*args, &block)
+      @res = @args ?  @obj.send(*@args, &@block) : @block.call(@obj)
+      @done = true
     end
-  end
 
+    @res.send(*args, &block)
+  end
 end
